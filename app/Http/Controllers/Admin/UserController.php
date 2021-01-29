320b47e393cb99ab3ce82d46619c89097935bdd3
weullermarcos@hotmail.com <weullermarcos@hotmail.com>
2021-01-29 09:53:11 -0300

    listagem de usuarios

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -14,8 +15,11 @@ class UserController extends Controller
      */
     public function index()
     {
+        //recuperando todos os usuários
+        $users = User::all();
 
-        return view('admin.users.index');
+        //retornando usuários recuperados como parametro
+        return view('admin.users.index', ['users' => $users]);
     }
 
     /**
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -6,13 +6,40 @@
 
 @section('content_header')
 
-    <h1>Meus usuários</h1>
+    <h1>
+        Meus usuários
+        <a href="{{route('users.create')}}" class="btn btn-sm btn-success">Novo Usuário</a>
+    </h1>
 
 @endsection
 
 @section('content')
 
+    <table class="table table-hover">
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Ações</th>
+        </tr>
 
+        @foreach($users as $user)
+
+            <tr>
+                <td>{{$user->id}}</td>
+                <td>{{$user->name}}</td>
+                <td>{{$user->email}}</td>
+                <td>{{$user->id}}</td>
+                <td>
+                    <a href="{{route('users.edit', ['user' => $user->id])}}" class="btn btn-sm btn-info">Editar</a>
+                    <a href="{{route('users.destroy', ['user' => $user->id])}}" class="btn btn-sm btn-danger">Excluir</a>
+                </td>
+            </tr>
+
+        @endforeach
+
+
+    </table>
 
 @endsection
 
