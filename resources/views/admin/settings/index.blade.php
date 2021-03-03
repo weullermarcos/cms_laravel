e4483ba015fc81a59994dc5ae4fabb994a8d7c82
weullermarcos@hotmail.com <weullermarcos@hotmail.com>
2021-03-03 11:08:02 -0300

    Model e Controller de páginas

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -59,14 +59,14 @@
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label"> Cor do Fundo: </label>
                     <div class="col-sm-10 ">
-                        <input type="color" name="bgcolor" value="{{$settings['bgcolor']}}" class="form-control">
+                        <input type="color" name="bgcolor" value="{{$settings['bgcolor']}}" class="form-control" style="width: 70px">
                     </div>
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label"> Cor do Texto: </label>
                     <div class="col-sm-10 ">
-                        <input type="color" name="textcolor" value="{{$settings['textcolor']}}" class="form-control">
+                        <input type="color" name="textcolor" value="{{$settings['textcolor']}}" class="form-control" style="width: 70px">
                     </div>
                 </div>
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use App\Http\Controllers\Admin\PageController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Site\HomeController;
@@ -44,6 +45,9 @@ Route::prefix('painel')->group(function (){
 
     //equivale ao crud para rotas de usuários
     Route::resource('users', UserController::class);
+
+    //equivale ao crud para rotas de páginas
+    Route::resource('pages', PageController::class);
 
     Route::get('profile', [ProfileController::class, 'index'])->name('profile');
 
